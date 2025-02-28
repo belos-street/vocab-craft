8a77c3641bce3c015df1add535dc4cb89ab9f917
belos-street <869135421@qq.com>
2025-02-28 22:16:36 +0800

    frist commit

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type Chapter = {
+    identity: string
+    title: string
+    content: string
+    words: string[]
+}
+
+const str = '人生的[]，如果仅在于[]成功，得到的快乐[]并不会多。[]带给你成就感的是成长。[]自身的[], da 未知的[]，在不断[]自己的过程中，更能[]到由衷的[]和人生的[]。'
+
+export type local = {
+    achieved: boolean
+}
