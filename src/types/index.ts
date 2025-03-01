b53a29f8b4c7e6b88547b0c066766fb5717d7a92
belos-street <869135421@qq.com>
2025-03-01 23:44:28 +0800

    init

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export type Chapter = {
     words: string[]
 }
 
-const str = '人生的[]，如果仅在于[]成功，得到的快乐[]并不会多。[]带给你成就感的是成长。[]自身的[], da 未知的[]，在不断[]自己的过程中，更能[]到由衷的[]和人生的[]。'
+const str = '人生的[significance]，如果仅在于[pursue]成功，得到的快乐[perhaps]并不会多。[indeed]带给你成就感的是成长。[conquer]自身的[defect]，[quest]未知的[domain]，在不断[polish]自己的过程中，更能[comprehend]到由衷的[joy]和人生的[merit]。'
 
 export type local = {
     achieved: boolean
