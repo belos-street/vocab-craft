b98a10b27cf05ebc9c1cc44d5a907bf70e462172
belos-street <869135421@qq.com>
2025-03-02 23:55:50 +0800

    feat: 初始化路由

---
+++ src/app/chapter/page.tsx
@@ -0,0 +1,7 @@
+export default function Chapter() {
+  return (
+    <div className="w-screen h-screen flex flex-col gap-12">
+      <main className="flex flex-col gap-12">123</main>
+    </div>
+  )
+}
